7f2bf9e5ff001b5d54fe31730af33a52cf85065b
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-07-28 22:19:14 +0000

    done refactoring needed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const taxRate = 0.055;
-const tax = 0.00;
+let tax = 0.00;
 let subTotal = 0.00;
-const total = 0.00;
+let total = 0.00;
 
 let item1 = 0.00;
 let item2 = 0.00;
@@ -16,7 +16,11 @@ while (isValidInput) {
   item3 = parseFloat(prompt("Enter price for item 3: "));
   if (!isNaN(item1) > 0 && !isNaN(item2) > 0 && !isNaN(item3) > 0) {
     subTotal += item1 + item2 + item3;
-    alert(subTotal);
+    tax = subTotal * taxRate;
+    total =  tax + subTotal;
+    console.log(`Yout subtotal is: ${subTotal}`);
+    console.log(`Your tax is: ${tax}`);
+    console.log(`Your total is: ${total}`);
     isValidInput = false;
   } else {
     alert("Invalid input. Please enter a valid number.");
