7a810ab93cb61f9f059a1f32e61ceb112b4d575d
WavyCoder <53162178+DatravousOdds@users.noreply.github.com>
2024-08-02 01:45:53 +0000

    logic implemented

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ const taxRate = 0.055;
 let tax = 0.00;
 let subTotal = 0.00;
 let total = 0.00;
+let itemCounter = 1;
 
-let item1 = 0.00;
-let item2 = 0.00;
-let item3 = 0.00;
+let input = ""
+let quanitity = 0;
 
 
-let isValidInput = true;
+while (input !== "stop") {
+  input = prompt("Enter the price of item " + itemCounter + " or Q = exit:");
+  quanitity = prompt("Enter the quanitity of item " + itemCounter + " or Q = exit:");
 
-while (isValidInput) {
-  item1 = parseFloat(prompt("Enter price for item 1: "));
-  item2 = parseFloat(prompt("Enter price for item 2: "));
-  item3 = parseFloat(prompt("Enter price for item 3: "));
-  if (!isNaN(item1) > 0 && !isNaN(item2) > 0 && !isNaN(item3) > 0) {
-    subTotal += item1 + item2 + item3;
-    tax = subTotal * taxRate;
-    total =  tax + subTotal;
-    console.log(`Yout subtotal is: ${subTotal}`);
-    console.log(`Your tax is: ${tax}`);
-    console.log(`Your total is: ${total}`);
-    isValidInput = false;
+  if (input === "Q" || quanitity === "Q") {
+    break;
   } else {
-    alert("Invalid input. Please enter a valid number.");
-  }
+    // add one to the counter
+    itemCounter++;
+    subTotal += input * quanitity;
+    // compute tax
+    tax = subTotal * taxRate;
+    // compute total
+    total = subTotal + tax;
 
+  }
 }
+
+console.log("Subtotal: $" + subTotal.toFixed(2));
+console.log("Tax: $" + tax.toFixed(2));
+console.log("Total: $" + total.toFixed(2));
